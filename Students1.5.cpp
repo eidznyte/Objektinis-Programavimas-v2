6d501224703ecf1c26f922b88088ba1acb3e1069
eidznyte <144419332+eidznyte@users.noreply.github.com>
2023-12-15 00:28:06 +0200

    Update Students1.5.cpp

--- Students1.5.cpp
+++ Students1.5.cpp
@@ -9,7 +9,6 @@
 #include <limits>
 #include <chrono>
 
-// Global variables for time measurement
 std::chrono::high_resolution_clock::time_point start;
 std::chrono::high_resolution_clock::time_point end;
 
@@ -45,6 +44,9 @@ void readFromFile(std::vector<Student>& students, const std::string& filename) {
         return;
     }
 
+    std::string headerLine;
+    std::getline(file, headerLine);
+
     Student student;
     while (file >> student) {
         students.push_back(student);
@@ -53,51 +55,89 @@ void readFromFile(std::vector<Student>& students, const std::string& filename) {
     file.close();
 }
 
+
+
+void writeToFile(const std::vector<Student>& students, const std::string& filename) {
+    std::cout << "Writing " << students.size() << " students to file: " << filename << std::endl;
+    std::ofstream outFile(filename);
+    if (!outFile.is_open()) {
+        throw std::runtime_error("Could not open file " + filename + " for writing.");
+    }
+
+    outFile << std::left << std::setw(15) << "Name"
+        << std::setw(15) << "Surname"
+        << std::setw(15) << "Final(Avg)"
+        << std::setw(15) << "Final(Med)" << std::endl;
+    outFile << std::string(60, '-') << std::endl;
+
+    for (const Student& student : students) {
+        outFile << std::left
+            << std::setw(15) << student.getName()
+            << std::setw(15) << student.getSurname()
+            << std::setw(15) << std::fixed << std::setprecision(2) << student.getFinalScoreAvg()
+            << std::setw(15) << std::fixed << std::setprecision(2) << student.getFinalScoreMed() << std::endl;
+    }
+
+    outFile.close();
+}
+
 void inputStudentsManually(std::vector<Student>& students) {
-    int numStudents;
     std::cout << "Enter the number of students: ";
-    std::cin >> numStudents;
+    int numStudents;
+    while (!(std::cin >> numStudents) || numStudents <= 0) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input! Please enter a positive integer for the number of students: ";
+    }
+    std::cin.ignore(); 
 
     for (int i = 0; i < numStudents; ++i) {
-        Student student;
-        std::cin >> student;  // This will call the overloaded operator>> for Student
+        std::cout << "Enter details for student " << i + 1 << ":\n";
+
+        std::string name, surname;
+        std::cout << "Name: ";
+        std::cin >> name;
+        std::cout << "Surname: ";
+        std::cin >> surname;
+        Student student(name, surname);
+
+        std::cout << "Enter homework scores (end with a non-numeric input, e.g., 'done'):\n";
+        int score;
+        std::vector<int> scores;
+        while (std::cin >> score) {
+            scores.push_back(score);
+        }
+
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::cout << "Enter exam score: ";
+        std::cin >> score;
+        student.setExam(score);
+        student.setHomeworks(scores);
+
+        student.setFinalScoreAvg(calculateAverage(student.getHomeworks()));
+        student.setFinalScoreMed(calculateMedian(student.getHomeworks()));
+
         students.push_back(student);
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 }
 
-std::ostream& operator<<(std::ostream& out, const Student& student) {
-    out << "Name: " << student.getName()
-        << ", Surname: " << student.getSurname()
-        << ", Exam: " << student.getExam()
-        << ", Average Score: " << student.getFinalScoreAvg()
-        << ", Median Score: " << student.getFinalScoreMed();
-    // You can format the output as needed and include other student details
-    return out;
-}
 
-std::istream& operator>>(std::istream& in, Student& student) {
-    std::string name, surname;
-    int homeworkScore, examScore;
+
+void generateRandomScores(Student& student) {
+    const int numHomeworks = rand() % 20 + 1;
     std::vector<int> homeworks;
-
-    in >> name >> surname;
-    student.setName(name);
-    student.setSurname(surname);
-
-    // Example reading format, adjust based on your actual data format
-    while (in >> homeworkScore) {
-        homeworks.push_back(homeworkScore);
+    for (int i = 0; i < numHomeworks; ++i) {
+        homeworks.push_back(rand() % 10 + 1);
     }
-
-    in.clear(); // Clear any error flags
-    in >> examScore; // Read the exam score
-
     student.setHomeworks(homeworks);
-    student.setExam(examScore);
-    student.setFinalScoreAvg(calculateAverage(homeworks));
-    student.setFinalScoreMed(calculateMedian(homeworks));
+    student.setExam(rand() % 10 + 1);
 
-    return in;
+    student.setFinalScoreAvg(calculateAverage(student.getHomeworks()));
+    student.setFinalScoreMed(calculateMedian(student.getHomeworks()));
 }
 
 void generateFile(const std::string& filename, int numStudents) {
@@ -126,54 +166,14 @@ void generateFile(const std::string& filename, int numStudents) {
     outFile.close();
 }
 
-void writeToFile(const std::vector<Student>& students, const std::string& filename) {
-    std::ofstream outFile(filename);
-    if (!outFile.is_open()) {
-        throw std::runtime_error("Could not open file " + filename + " for writing.");
-    }
-
-    outFile << std::left
-        << std::setw(15) << "Name"
-        << std::setw(15) << "Surname"
-        << std::setw(15) << "Final(Avg)"
-        << std::setw(15) << "Final(Med)" << std::endl;
-    outFile << std::string(60, '-') << std::endl;
-
-    for (const Student& student : students) {
-        outFile << std::left
-            << std::setw(15) << student.getName()
-            << std::setw(15) << student.getSurname()
-            << std::setw(15) << std::fixed << std::setprecision(2) << student.getFinalScoreAvg()
-            << std::setw(15) << std::fixed << std::setprecision(2) << student.getFinalScoreMed() << std::endl;
-    }
-    outFile.close();
-}
-
-void Student::displayInfo() const {
-    std::cout << "Student Name: " << name << ", Surname: " << surname << std::endl;
-    std::cout << "Homework Scores: ";
-    for (const auto& score : homeworks) {
-        std::cout << score << " ";
-    }
-    std::cout << "\nExam Score: " << exam << std::endl;
-    std::cout << "Average Score: " << finalScoreAvg << ", Median Score: " << finalScoreMed << std::endl;
-}
-
-void generateRandomScores(Student& student) {
-    const int numHomeworks = rand() % 20 + 1;
-    std::vector<int> homeworks;
-    for (int i = 0; i < numHomeworks; ++i) {
-        homeworks.push_back(rand() % 10 + 1);
-    }
-    student.setHomeworks(homeworks);
-    student.setExam(rand() % 10 + 1);
-}
-
 void categorizeStudents(std::vector<Student>& students, std::vector<Student>& dummies) {
     auto start = std::chrono::high_resolution_clock::now();
 
+    std::cout << "Starting categorization. Initial student count: " << students.size() << std::endl;
+
     for (auto it = students.begin(); it != students.end();) {
         if (it->getFinalScoreAvg() < 5.0) {
+            std::cout << "Moving " << it->getName() << " to dummies." << std::endl;
             dummies.push_back(*it);
             it = students.erase(it);
         }
@@ -182,23 +182,70 @@ void categorizeStudents(std::vector<Student>& students, std::vector<Student>& du
         }
     }
 
+    std::cout << "Categorization complete. Smart count: " << students.size() << ", Dummies count: " << dummies.size() << std::endl;
+
     auto end = std::chrono::high_resolution_clock::now();
     displayDuration(start, end, "Categorizing students with vectors");
 }
 
-void categorizeStudentsremove(std::vector<Student>& students, std::vector<Student>& dummies) {
-    auto start = std::chrono::high_resolution_clock::now();
+void displayStudents(const std::vector<Student>& students) {
+    for (const Student& student : students) {
+        std::cout << student << std::endl;
+    }
+}
 
-    auto newEnd = std::remove_if(students.begin(), students.end(),
-        [&dummies](const Student& student) {
-            bool isDummie = student.getFinalScoreAvg() < 5.0;
-            if (isDummie) {
-                dummies.push_back(student);
-            }
-            return isDummie;
-        });
-    students.erase(newEnd, students.end());
 
-    auto end = std::chrono::high_resolution_clock::now();
+std::istream& operator>>(std::istream& in, Student& student) {
+    in >> std::ws; 
 
+    in >> student.name >> student.surname;
+
+    student.homeworks.clear();
+    int score;
+    while (in.peek() != '\n' && in >> score) {
+        student.homeworks.push_back(score);
+    }
+
+    if (!student.homeworks.empty()) {
+        student.exam = student.homeworks.back();
+        student.homeworks.pop_back();
+    }
+    else {
+        student.exam = 0;
+    }
+
+    student.setFinalScoreAvg(calculateAverage(student.homeworks));
+    student.setFinalScoreMed(calculateMedian(student.homeworks));
+
+    
+    in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    return in;
+}
+
+
+
+std::ostream& operator<<(std::ostream& out, const Student& student) {
+    out << "Name: " << student.getName() << ", Surname: " << student.getSurname()
+        << ", Final Avg Score: " << student.getFinalScoreAvg()
+        << ", Final Med Score: " << student.getFinalScoreMed();
+    return out;
+}
+
+
+
+
+void Student::displayInfo() const {
+    std::cout << "Name: " << name
+        << ", Surname: " << surname
+        << ", Exam Score: " << exam
+        << ", Final Average Score: " << finalScoreAvg
+        << ", Final Median Score: " << finalScoreMed
+        << std::endl;
+
+    std::cout << "Homework Scores: ";
+    for (const auto& score : homeworks) {
+        std::cout << score << " ";
+    }
+    std::cout << std::endl;
 }
