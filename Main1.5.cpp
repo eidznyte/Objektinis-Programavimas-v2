b2f95496c020ff4eea8555288ed432fc6a6186a7
eidznyte <144419332+eidznyte@users.noreply.github.com>
2023-12-15 00:26:34 +0200

    Update Main1.5.cpp

--- Main1.5.cpp
+++ Main1.5.cpp
@@ -8,19 +8,24 @@
 int main() {
     std::vector<Student> studentsVector, dummiesVector;
 
-    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-    std::chrono::duration<double> duration;
-
-    std::cout << "Would you like to: \n"
-        << "1. Input data manually \n"
-        << "2. Read data from a file \n"
-        << "3. Generate a random student list file\n"
-        << "Choose an option (1/2/3): ";
     int choice;
-    std::cin >> choice;
+    while (true) {
+        std::cout << "Would you like to: \n"
+            << "1. Input data manually \n"
+            << "2. Read data from a file \n"
+            << "3. Generate a random student list file\n"
+            << "Choose an option (1/2/3): ";
+        std::cin >> choice;
+        if (std::cin.fail() || choice < 1 || choice > 3) {
+            std::cerr << "Invalid choice, please try again.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        break;
+    }
 
     std::string filename;
-    start = std::chrono::high_resolution_clock::now();
     try {
         switch (choice) {
         case 1:
@@ -64,9 +69,19 @@ int main() {
         return 1;
     }
 
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-    std::cout << "\nTime taken for operation: " << duration.count() << " seconds." << std::endl;
+    try {
+        auto start = std::chrono::high_resolution_clock::now();
+        categorizeStudents(studentsVector, dummiesVector);
+        writeToFile(dummiesVector, "dummies_vector.txt");
+        writeToFile(studentsVector, "smart_vector.txt");
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+        std::cout << "\nTime taken for file operations: " << duration.count() << " seconds." << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error during file operations: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
